060bc93ca6e209b1d8f76fbd15756ec74ba6c553
AvrisNox <noahd@mtu.edu>
2020-08-11 13:08:58 -0400

    Basic framework in building order
    Basic framework (which literally just creates and shows a stage) is functional. Now anyone can download and run the gradle to make it do the thing. Running "build" will build the application into a jar. Running "run" will run the application from IntelliJ (or the IDE of choice).

---
+++ src/main/java/com/avrisnox/roll420/MainApplication.java
@@ -0,0 +1,11 @@
+package com.avrisnox.roll420;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MainApplication extends Application {
+	@Override
+	public void start(Stage stage) throws Exception {
+		stage.show();
+	}
+}
